f36ec5655da7c38cca493da9f55419657979a580
Ncorp30 <Nisha.Kumari1@nathcorp.com>
2026-07-15 12:54:25 +0530

    .psm1

---
+++ MyTools.psm1
@@ -0,0 +1,69 @@
+# MyTools.psm1
+
+function Get-SystemInfo {
+    <#
+    .SYNOPSIS
+    Gets basic system information.
+    #>
+
+    [CmdletBinding()]
+    param()
+
+    [PSCustomObject]@{
+        ComputerName = $env:COMPUTERNAME
+        UserName     = $env:USERNAME
+        OS           = (Get-CimInstance Win32_OperatingSystem).Caption
+        LastBootTime = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+    }
+}
+
+
+function Test-FileExists {
+    <#
+    .SYNOPSIS
+    Checks whether a file exists.
+
+    .PARAMETER Path
+    File path to check.
+    #>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Path
+    )
+
+    if (Test-Path -Path $Path) {
+        return $true
+    }
+
+    return $false
+}
+
+
+function Write-LogMessage {
+    <#
+    .SYNOPSIS
+    Writes a formatted log message.
+    #>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Message,
+
+        [ValidateSet("INFO","WARNING","ERROR")]
+        [string]$Level = "INFO"
+    )
+
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    Write-Host "[$timestamp][$Level] $Message"
+}
+
+
+# Export module functions
+Export-ModuleMember -Function `
+    Get-SystemInfo, `
+    Test-FileExists, `
+    Write-LogMessage
